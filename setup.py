d99a24e15835a117f3b70f842581e72070b57ad2
Fredrik Thulin <fredrik@thulin.net>
2017-02-01 19:31:37 +0100

    fix

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
+here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 
 testing_extras = [
